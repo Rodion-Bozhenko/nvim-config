02c4c3268e8f4617eeeda9be92510b3761dab0f8
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-10-22 14:36:02 +0200

    Add persistence for undo tree

--- lua/rodion/plugins/undo-tree.lua
+++ lua/rodion/plugins/undo-tree.lua
@@ -1,1 +1,12 @@
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+if vim.fn.has("persistent_undo") == 1 then
+	local target_path = vim.fn.expand("~/.undodir")
+
+	if not vim.fn.isdirectory(target_path) then
+		vim.fn.mkdir(target_path, "p", 0700)
+	end
+
+	vim.o.undodir = target_path
+	vim.o.undofile = true
+end
