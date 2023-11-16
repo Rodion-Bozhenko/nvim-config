eefbdddb71bb06fdb0804ceab69ec22ebbf86a49
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-11-16 18:06:01 +0100

    Add ignore files and disable treesitter in telescope, so it'll work faster

--- lua/rodion/plugins/telescope.lua
+++ lua/rodion/plugins/telescope.lua
@@ -1,27 +1,42 @@
--- import telescope plugin safely
 local telescope_setup, telescope = pcall(require, "telescope")
 if not telescope_setup then
-  return
+	return
 end
 
--- import telescope actions safely
 local actions_setup, actions = pcall(require, "telescope.actions")
 if not actions_setup then
-  return
+	return
 end
 
--- configure telescope
 telescope.setup({
-  -- configure custom mappings
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-        ["<C-j>"] = actions.move_selection_next, -- move to next result
-        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-      },
-    },
-  },
+	defaults = {
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+			},
+		},
+		file_ignore_patterns = {
+			"yarn.lock",
+			"package-lock.json",
+			"node_modules/*",
+			".terraform/*",
+			"*.log",
+			"dist/*",
+			"build/*",
+		},
+		preview = {
+			treesitter = false,
+			filesize_limit = 1,
+			highlight_limit = false,
+		},
+	},
+	pickers = {
+		live_grep = {
+			min_chars = 3,
+		},
+	},
 })
 
 telescope.load_extension("fzf")
